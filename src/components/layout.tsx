42c5bd7830b2b1f373bcc1c0f92e6c49a468d985
nrsimonelli <nrsimonelli@gmail.com>
2024-02-14 13:20:30 -0600

    applied flex layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom'
 
 export const Layout = () => {
   return (
-    <div className='min-h-screen'>
+    <div className='flex flex-col min-h-screen space-y-8'>
       <TopNav />
-      <div className='container flex flex-1'>
+      <div className='container flex flex-col flex-1'>
         <Outlet />
       </div>
     </div>
