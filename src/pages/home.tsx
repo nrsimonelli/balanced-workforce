61149f2856d7550683f4364818d4c6b099ad0ecf
nrsimonelli <nrsimonelli@gmail.com>
2024-02-14 13:21:23 -0600

    placeholder text

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,8 @@
 export const Home = () => {
-  return <div>coming soon</div>
+  return (
+    <div className='flex flex-col items-center justify-start flex-1 space-y-8'>
+      <h1>Welcome to scythe matchups</h1>
+      <p>Content TBD</p>
+    </div>
+  )
 }
